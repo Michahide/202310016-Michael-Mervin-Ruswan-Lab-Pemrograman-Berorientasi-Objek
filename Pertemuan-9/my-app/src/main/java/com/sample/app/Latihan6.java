9a19ba8b76f35b21ebf361463656fc5c76e35f4a
Michahide <73429490+Michahide@users.noreply.github.com>
2021-11-22 22:25:29 +0700

    pertemuan-9

--- Pertemuan-9/my-app/src/main/java/com/sample/app/Latihan6.java
+++ Pertemuan-9/my-app/src/main/java/com/sample/app/Latihan6.java
@@ -6,33 +6,33 @@ public class Latihan6 {
     public static String nama;
     public static String label;
     public static void main(String[] args) {
-        nama = JOptionPane.showInputDialog(null, "Masukkan namamu");
-        while (nama.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Field tidak boleh kosong", "Peringatan", JOptionPane.ERROR_MESSAGE);
+        nama = JOptionPane.showInputDialog(null, "Masukkan namamu"); //keluarkan input dialog untuk menanyakan nama
+        while (nama.isEmpty()) { //Looping untuk error field/inputan yang kosong
+            JOptionPane.showMessageDialog(null, "Field tidak boleh kosong", "Peringatan", JOptionPane.ERROR_MESSAGE); //keluarkan pesan error
             nama = JOptionPane.showInputDialog(null, "Masukkan namamu");
         }
 
-        Latihan6 myObj = new Latihan6();
+        Latihan6 myObj = new Latihan6(); //buat objek baru bernama myObj
         int inputButton = JOptionPane.showConfirmDialog(null, 
             "Apakah kamu suka burger?",
             "Pertanyaan",
             JOptionPane.YES_NO_CANCEL_OPTION,
-            JOptionPane.QUESTION_MESSAGE);
-        if (inputButton == 0) {
-            myObj.sukaBurger();
-        } else if (inputButton == 1) {
+            JOptionPane.QUESTION_MESSAGE); //buat confirm dialog dengan pertanyaan "Apakah kamu suka burger"
+        if (inputButton == 0) { //kalau pilih Yes
+            myObj.sukaBurger(); //memanggil fungsi sukaBurger
+        } else if (inputButton == 1) { //kalau pilih no
             label = nama + " tidak suka burger";
-            JOptionPane.showMessageDialog(null, label, "Pilihan", JOptionPane.INFORMATION_MESSAGE);
-            int pilihanSuka = JOptionPane.showConfirmDialog(null, "Apakah kamu yakin?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-            if (pilihanSuka == 0) {
-                myObj.sukaBurger();
+            JOptionPane.showMessageDialog(null, label, "Pilihan", JOptionPane.INFORMATION_MESSAGE); //keluarkan pesan yang menyatakan user tidak suka burger
+            int pilihanSuka = JOptionPane.showConfirmDialog(null, "Apakah kamu yakin?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE); //keluarkan pesan untuk menanyakan kepastian user
+            if (pilihanSuka == 0) { //kalau pilih yes untuk pertanyaan "Apakah kamu yakin?"
+                myObj.sukaBurger(); //panggil fungsi sukaBurger
             }
 
-        } else if (inputButton == 2) {
+        } else if (inputButton == 2) { //kalau pilih cancel  untuk pertanyaan "Apakah kamu suka burger?"
             JOptionPane.showMessageDialog(null, "Tidak memilih", "Not choose anything :v", JOptionPane.INFORMATION_MESSAGE);
             int pilihanSuka = JOptionPane.showConfirmDialog(null, "Apakah kamu yakin?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-            if (pilihanSuka == 0) {
-                myObj.sukaBurger();
+            if (pilihanSuka == 0) { //kalau pilih yes untuk pertanyaan "Apakah kamu yakin?"
+                myObj.sukaBurger(); //panggil fungsi sukaBurger
             }
         }
         
@@ -41,17 +41,17 @@ public class Latihan6 {
     public void sukaBurger (){
         Latihan6 myObj = new Latihan6();
         String jumlahBurgerString= JOptionPane.showInputDialog(null,"Harga burger: Rp 38.000\nMasukkan jumlah burger yang dipesan");
-        while (myObj.isNumber(jumlahBurgerString) == false) {
+        while (myObj.isNumber(jumlahBurgerString) == false) { //membuat looping unutk error message terkait input yang dimasukkan user harus berupa angka
             JOptionPane.showMessageDialog(null, "Input yang dimasukkan harus berupa angka", "Error", JOptionPane.ERROR_MESSAGE);
             jumlahBurgerString= JOptionPane.showInputDialog(null,"Harga burger: Rp 38.000\nMasukkan jumlah burger yang dipesan");
         }
-        Double jumlahBurger = Double.parseDouble(jumlahBurgerString);
+        Double jumlahBurger = Double.parseDouble(jumlahBurgerString); //mengkonversi string yang berasal dari jumlahBurgerString
         Double hargaBurger = jumlahBurger*38000;
         String hargaBurgerString = hargaBurger+"";
         label = nama + " suka burger\nBurger yang dipesan: " + jumlahBurgerString + "\n Harga burger = " + hargaBurgerString ;
-        JOptionPane.showMessageDialog(null, label, "Pilihan", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, label, "Pilihan", JOptionPane.INFORMATION_MESSAGE); //mengeluarkan pesan dari variabel label
     }
-    public boolean isNumber(String jumlah) {
+    public boolean isNumber(String jumlah) { //membuat fungsi error handling/exception menggunakan try and catch
         try {
             Double x = Double.parseDouble(jumlah);
             return true;
